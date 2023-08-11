2dd4ecaa3397a26104548cc98fbd4345cfc4af39
Valdes32 <pro100kul@gmail.com>
2023-08-11 15:52:04 +0300

    done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+	const stack = [];
+	const bracketsMap = new Map(bracketsConfig);
+
+	for (const char of str) {
+			if (bracketsMap.has(char)) {
+					const openingBracket = bracketsMap.get(char);
+
+					if (openingBracket === char && stack[stack.length - 1] === char) {
+							stack.pop(); // Удаляем закрывающую скобку из стека, если она равна открывающей
+					} else {
+							stack.push(char);
+					}
+			} else {
+					const lastOpeningBracket = stack.pop();
+					if (bracketsMap.get(lastOpeningBracket) !== char) {
+							return false;
+					}
+			}
+	}
+
+	return stack.length === 0;
+};
